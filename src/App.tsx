5e21e4033da5f46edd0f76b247df510b2cfeab90
unknown <tareqhasan382@gmail.com>
2026-02-19 19:36:09 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+
+  return (
+   <div className=" w-full flex justify-center ">
+    <h1 className=" text-2xl font-bold ">Hello World!</h1>
+   </div>
+  )
+}
+
+export default App
