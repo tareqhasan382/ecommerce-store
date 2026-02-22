8c0bf50ee8a24ee00c8e881b953feec5abd998fb
unknown <tareqhasan382@gmail.com>
2026-02-22 16:51:39 +0600

    made hero text responsive

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,14 @@ function App() {
     <Container className="bg-amber-300">
 
       {/* Main hero area */}
-      <div className="w-full flex justify-center items-center h-[265px]">
-        <h1 className="font-rubik font-bold text-[200px] leading-[223.5px] tracking-normal uppercase text-center whitespace-nowrap">
-          DO IT <span className=" text-blue-500 ">RIGHT</span>
-        </h1>
-      </div>
+      <div className="w-full flex justify-center items-center h-[265px] px-4">
+  <h1 className="font-rubik font-extrabold uppercase text-center whitespace-nowrap
+                 text-[4rem] sm:text-[5rem] md:text-[8rem] lg:text-[11rem] xl:text-[11rem]
+                 leading-[3.5rem] sm:leading-[5.5rem] md:leading-[9rem] lg:leading-[15rem] xl:leading-[19rem]
+                 tracking-normal">
+    DO IT <span className="text-blue-500">RIGHT</span>
+  </h1>
+</div>
 
       {/* Header / Product area */}
       <Header />
